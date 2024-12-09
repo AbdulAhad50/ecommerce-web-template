db8a4d79a0b437e9111347a0169687f8292ce796
AbdulAhad50 <aabdulahad142@gmail.com>
2024-12-09 23:01:59 +0500

    Name change

--- src/app/components/Sooping.tsx
+++ src/app/components/Sooping.tsx
@@ -4,7 +4,7 @@ const SoopingCart = () => {
   return (
     <div className='w-[417px] h-[746px] bg-white'>
         <div className='flex w-[387px] h-[334px]'>
-            <h1>Shopping Cart</h1>
+            <h1>Shopping Crt</h1>
         </div>
 
     </div>
