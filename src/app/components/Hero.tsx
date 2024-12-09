47f556507bb32199536ffb363e822452355516f7
AbdulAhad50 <aabdulahad142@gmail.com>
2024-12-09 23:37:05 +0500

    and changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,21 +1,24 @@
-import Image from "next/image"
-const Hero = ({name,category}:any) => {
-  return (
-    <div className='relatives flex justify-center items-center mt-12 -w-[1440px] h-[316px]'>
-        <div className="heroSection flex flex-col items-center justify-center blur-[2px]">
-            <img src="/hero.jpg" alt=""  className="img"/>
-        </div>
+import Image from "next/image";
 
-        <div className="absolute flex flex-col justify-center items-center z-10">
-              <Image src="/logos.svg" alt="heroSection Logo" width={40} height={20} className=""/>
-                <h1 className="heroHeading">{name}</h1>
-                <p className="sh">Home {">"} <span className="category">{category}</span></p>
-        </div>
-
-       
-
-    </div>
-  )
+interface HeroProps {
+  name: string;
+  category: string;
 }
 
-export default Hero
+const Hero = ({ name, category }: HeroProps) => {
+  return (
+    <div className='relative flex justify-center items-center mt-12 w-[1440px] h-[316px]'>
+      <div className="heroSection flex flex-col items-center justify-center blur-[2px]">
+        <img src="/hero.jpg" alt="Hero Image" className="img" />
+      </div>
+
+      <div className="absolute flex flex-col justify-center items-center z-10">
+        <Image src="/logos.svg" alt="Hero Section Logo" width={40} height={20} />
+        <h1 className="heroHeading">{name}</h1>
+        <p className="sh">
+          Home {">"} <span className="category">{category}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
